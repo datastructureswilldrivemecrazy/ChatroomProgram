ef9d6e9a6930241c6ebe9a799cdae21e2be9545a
Bishal Sen <109077896+datastructureswilldrivemecrazy@users.noreply.github.com>
2022-09-07 19:34:16 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,20 +1,16 @@
 import threading
 import socket
-
 host = '127.0.0.1'
 port = 55555
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host , port))
 server.listen()
-
+#initializing two lists which will store the clients and the nicknames assigned to them 
 clients = []
 nicknames = []
-
 def broadcast (message):#sends the message to all the clients in the server
     for client in clients:
         client.send(message) 
-
 def handle(client):
     while True:
         try:
@@ -28,7 +24,6 @@ def handle(client):
             broadcast(f'{nickname} has left the chat'.encode('ascii'))
             nicknames.remove(nickname)    
             break
-
 def receive():
     while True:
         client, address = server.accept()
@@ -45,6 +40,8 @@ def receive():
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-
-print("go on, server is listening...")
+print("WELCOME   TO    BISHAL's   PRIVATE   CHATROOM")
+print("CONNECT YOURSELF AS A CLIENT AND AHAVE FUN!!!")
+print("__________________________________________________")
+print("server online......checking for clients")
 receive()
