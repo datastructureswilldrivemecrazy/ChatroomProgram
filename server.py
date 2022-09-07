6b65bb8cf3c4fcae6aa86e63ae5178164dcc553e
Bishal Sen <109077896+datastructureswilldrivemecrazy@users.noreply.github.com>
2022-09-07 19:14:58 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,50 @@
+import threading
+import socket
+
+host = '127.0.0.1'
+port = 55555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host , port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast (message):#sends the message to all the clients in the server
+    for client in clients:
+        client.send(message) 
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} has left the chat'.encode('ascii'))
+            nicknames.remove(nickname)    
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"connected with {str(address)}")
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f'{nickname} has joined the chat'.encode('ascii'))
+        client.send(f'connected to the server'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("go on, server is listening...")
+receive()
